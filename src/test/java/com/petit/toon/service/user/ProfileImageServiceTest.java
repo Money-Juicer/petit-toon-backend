0ea06f40af5c46564d3c3d1f28b426f92b647612
Hotoran <hotoran0410@gmail.com>
2023-08-03 01:23:04 +0900

    feat. ProfileImage, User, Cartoon Repository 및 Service 테스트

--- src/test/java/com/petit/toon/service/user/ProfileImageServiceTest.java
+++ src/test/java/com/petit/toon/service/user/ProfileImageServiceTest.java
@@ -5,6 +5,7 @@ import com.petit.toon.entity.user.User;
 import com.petit.toon.repository.user.ProfileImageRepository;
 import com.petit.toon.repository.user.UserRepository;
 import com.petit.toon.service.user.response.ProfileImageResponse;
+import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -40,11 +41,14 @@ class ProfileImageServiceTest {
     static Path tempDir;
     String absolutePath;
 
+    ProfileImage defaultImage;
+
     @BeforeEach
     void setup() {
         String path = "src/test/resources/sample-profile-images";
         absolutePath = new File(path).getAbsolutePath();
         profileImageService.setProfileImageDirectory(String.valueOf(tempDir));
+        defaultImage = profileImageRepository.findById(profileImageService.DEFAULT_PROFILE_IMAGE_ID).get();
     }
 
     @AfterEach
@@ -54,12 +58,12 @@ class ProfileImageServiceTest {
     }
 
     @Test
+    @Transactional
     @DisplayName("프로필 사진 업로드")
     void upload() throws IOException {
         //given
         User user1 = createUser("김영현");
         User user2 = createUser("김승환");
-        User user3 = createUser("이용우");
 
         MultipartFile file1 = new MockMultipartFile("sample1.png", "sample1.png", "multipart/form-data",
                 new FileInputStream(absolutePath + "/sample1.png"));
@@ -74,26 +78,23 @@ class ProfileImageServiceTest {
         ProfileImage profileImage1 = profileImageRepository.findById(response1.getProfileImageId()).get();
         ProfileImage profileImage2 = profileImageRepository.findById(response2.getProfileImageId()).get();
 
-        assertThat(profileImage1.getId()).isEqualTo(response1.getProfileImageId());
         assertThat(profileImage1.getFileName()).isEqualTo(response1.getProfileImageId() + ".png");
         assertThat(profileImage1.getOriginFileName()).isEqualTo("sample1.png");
         assertThat(profileImage1.getPath()).isEqualTo(tempDir.resolve(response1.getProfileImageId() + ".png").toString());
 
-        user1 = userRepository.findById(user1.getId()).get();
-        assertThat(user1.getProfileImageId()).isEqualTo(profileImage1.getId());
+        user1 = userRepository.findUserById(user1.getId()).get(); // fetch join
+        assertThat(user1.getProfileImage()).isEqualTo(profileImage1);
 
-        assertThat(profileImage2.getId()).isEqualTo(response2.getProfileImageId());
         assertThat(profileImage2.getFileName()).isEqualTo(response2.getProfileImageId() + ".png");
         assertThat(profileImage2.getOriginFileName()).isEqualTo("sample2.png");
         assertThat(profileImage2.getPath()).isEqualTo(tempDir.resolve(response2.getProfileImageId() + ".png").toString());
 
-        user2 = userRepository.findById(user2.getId()).get();
-        assertThat(user2.getProfileImageId()).isEqualTo(profileImage2.getId());
-
-        assertThat(user3.getProfileImageId()).isEqualTo(0l);
+        user2 = userRepository.findUserById(user2.getId()).get();
+        assertThat(user2.getProfileImage()).isEqualTo(profileImage2);
     }
 
     @Test
+    @Transactional
     @DisplayName("Default 이미지로 변경")
     void updateToDefault() throws IOException {
         //given
@@ -103,22 +104,24 @@ class ProfileImageServiceTest {
 
         ProfileImageResponse response = profileImageService.upload(user1.getId(), file1);
         ProfileImage profileImage = profileImageRepository.findById(response.getProfileImageId()).get();
-        user1 = userRepository.findById(user1.getId()).get();
+        user1 = userRepository.findUserById(user1.getId()).get();
 
         //when
         profileImageService.updateToDefault(user1.getId());
 
         //then
-        user1 = userRepository.findById(user1.getId()).get();
-        assertThat(user1.getProfileImageId()).isEqualTo(0l);
+        user1 = userRepository.findUserById(user1.getId()).get();
+        assertThat(user1.getProfileImage().getId()).isEqualTo(ProfileImageService.DEFAULT_PROFILE_IMAGE_ID);
         assertThat(profileImageRepository.findById(profileImage.getId())).isEmpty();
 
     }
 
     private User createUser(String name) {
-        return userRepository.save(
+        User user = userRepository.save(
                 User.builder()
                         .name(name)
                         .build());
+        user.setProfileImage(defaultImage);
+        return userRepository.save(user);
     }
 }
