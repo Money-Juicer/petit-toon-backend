1bd9b7e10fc7858e01aee1af48689c21a8dcd15f
timel2ss <dyddn0823@hanyang.ac.kr>
2023-08-04 04:43:20 +0900

    feat: 좋아요/싫어요 기능 구현

---
+++ src/main/java/com/petit/toon/repository/cartoon/DislikeRepository.java
@@ -0,0 +1,14 @@
+package com.petit.toon.repository.cartoon;
+
+import com.petit.toon.entity.cartoon.Dislike;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface DislikeRepository extends JpaRepository<Dislike, Long> {
+    List<Dislike> findByCartoonId(long cartoonId);
+
+    @Query("select dl.user.id from Dislike dl where dl.cartoon.id = :cartoonId")
+    List<Long> findUserIdByCartoonId(long cartoonId);
+}
