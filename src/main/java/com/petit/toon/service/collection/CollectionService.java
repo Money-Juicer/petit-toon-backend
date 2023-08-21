5350ea134cee9fbfa480f143fd66cc93a83e6dcc
Hotoran <hotoran0410@gmail.com>
2023-08-21 12:46:23 +0900

    북마크, 콜렉션 DTO 및 서비스 구현

---
+++ src/main/java/com/petit/toon/service/collection/CollectionService.java
@@ -0,0 +1,100 @@
+package com.petit.toon.service.collection;
+
+import com.petit.toon.entity.cartoon.Cartoon;
+import com.petit.toon.entity.collection.Bookmark;
+import com.petit.toon.entity.collection.Collection;
+import com.petit.toon.entity.user.User;
+import com.petit.toon.repository.cartoon.CartoonRepository;
+import com.petit.toon.repository.collection.BookmarkRepository;
+import com.petit.toon.repository.collection.CollectionRepository;
+import com.petit.toon.repository.user.UserRepository;
+import com.petit.toon.service.collection.response.*;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CollectionService {
+    private final CollectionRepository collectionRepository;
+    private final BookmarkRepository bookmarkRepository;
+    private final UserRepository userRepository;
+    private final CartoonRepository cartoonRepository;
+
+
+    /**
+     * Collection 생성 서비스
+     */
+    public CollectionResponse createCollection(long userId, String title, boolean closed) {
+        User user = userRepository.findUserById(userId)
+                .orElseThrow(() -> new RuntimeException("User not found. id: " + userId));
+
+        Collection savedCollection = collectionRepository.save(Collection.builder()
+                .user(user)
+                .title(title)
+                .closed(closed)
+                .build());
+
+        return new CollectionResponse(savedCollection.getId());
+    }
+
+    /**
+     * Bookmark 생성
+     */
+    public BookmarkResponse createBookmark(long collectionId, long cartoonId) {
+        Collection collection = collectionRepository.findById(collectionId)
+                .orElseThrow(() -> new RuntimeException("Collection not found. id: " + collectionId));
+
+        Cartoon cartoon = cartoonRepository.findCartoonById(cartoonId)
+                .orElseThrow(() -> new RuntimeException("Cartoon not found. id: " + cartoonId));
+
+        Bookmark bookmark = bookmarkRepository.save(Bookmark.builder()
+                .collection(collection)
+                .cartoon(cartoon)
+                .build());
+
+        return new BookmarkResponse(bookmark.getId());
+    }
+
+    /**
+     * Author의 Collection List 조회
+     */
+    public CollectionInfoListResponse viewCollectionList(long authorId, boolean isSelf) {
+        List<Collection> collections = collectionRepository.findCollectionsByUserId(authorId);
+        if (isSelf) {
+            List<CollectionInfoResponse> responseList = collections.stream()
+                    .map(CollectionInfoResponse::of)
+                    .toList();
+            return new CollectionInfoListResponse(responseList);
+        }
+        List<CollectionInfoResponse> openedResponseList = collections.stream()
+                .filter(c -> !c.isClosed())
+                .map(CollectionInfoResponse::of)
+                .toList();
+        return new CollectionInfoListResponse(openedResponseList);
+    }
+
+    /**
+     * Collection의 Bookmark List 조회
+     */
+    public BookmarkInfoListResponse viewBookmarks(long collectionId, Pageable pageable) {
+
+        List<Bookmark> bookmarks = bookmarkRepository.findBookmarksByCollectionId(collectionId, pageable);
+        List<BookmarkInfoResponse> bookmarkInfos = bookmarks.stream()
+                .map(BookmarkInfoResponse::of)
+                .toList();
+        return new BookmarkInfoListResponse(bookmarkInfos);
+    }
+
+    public void removeCollection(long collectionId) {
+        collectionRepository.deleteById(collectionId);
+    }
+
+    public void removeBookmark(long bookmarkId) {
+        bookmarkRepository.deleteById(bookmarkId);
+    }
+}
