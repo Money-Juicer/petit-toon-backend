1cb48abc06b99ba5df3a65d2de4eb4e756cfac78
Hotoran <hotoran0410@gmail.com>
2023-09-01 23:45:08 +0900

    feat: 비동기 설정 파일 추가

---
+++ src/main/java/com/petit/toon/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package com.petit.toon.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+    private final int CORE_POOL_SIZE = 3;
+    private final int MAX_POOL_SIZE = 20;
+
+    @Bean(name = "threadPoolTaskExecutor")
+    public Executor threadPoolTaskExecutor() {
+        ThreadPoolTaskExecutor taskExecutor = new ThreadPoolTaskExecutor();
+        taskExecutor.setCorePoolSize(CORE_POOL_SIZE);
+        taskExecutor.setMaxPoolSize(MAX_POOL_SIZE);
+        taskExecutor.initialize();
+        return taskExecutor;
+    }
+}
