9bc42a5bdb88e3f54f6cf5af5d51d2538b6c038c
Hotoran <hotoran0410@gmail.com>
2023-09-03 19:51:58 +0900

    fix: 컬렉션 Response 수정

--- src/main/java/com/petit/toon/service/collection/response/CollectionInfoResponse.java
+++ src/main/java/com/petit/toon/service/collection/response/CollectionInfoResponse.java
@@ -4,18 +4,22 @@ import com.petit.toon.entity.collection.Collection;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+
 @Getter
 public class CollectionInfoResponse {
 
     private long id;
     private String title;
     private boolean closed;
+    private List<BookmarkInfoResponse> bookmarkInfos;
 
     @Builder
-    private CollectionInfoResponse(long id, String title, boolean closed) {
+    private CollectionInfoResponse(long id, String title, boolean closed, List<BookmarkInfoResponse> bookmarkInfos) {
         this.id = id;
         this.title = title;
         this.closed = closed;
+        this.bookmarkInfos = bookmarkInfos;
     }
 
     public static CollectionInfoResponse of(Collection collection) {
@@ -23,7 +27,10 @@ public class CollectionInfoResponse {
                 .id(collection.getId())
                 .title(collection.getTitle())
                 .closed(collection.isClosed())
+                .bookmarkInfos(collection.getBookmarks()
+                        .subList(0, Math.min(3, collection.getBookmarks().size()))
+                        .stream().map(BookmarkInfoResponse::of)
+                        .toList())
                 .build();
     }
-
 }
