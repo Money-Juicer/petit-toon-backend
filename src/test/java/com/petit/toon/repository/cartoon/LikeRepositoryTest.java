b60f87087563e4e8bf19c275b81ceb61a061f550
Hotoran <hotoran0410@gmail.com>
2023-09-05 16:31:02 +0900

    fix: 의존성 추가

--- src/test/java/com/petit/toon/repository/cartoon/LikeRepositoryTest.java
+++ src/test/java/com/petit/toon/repository/cartoon/LikeRepositoryTest.java
@@ -8,6 +8,7 @@ import com.petit.toon.repository.user.UserRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,6 +20,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DataJpaTest
 @ActiveProfiles("test")
 @Import(QueryDslConfig.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class LikeRepositoryTest {
 
     @Autowired
