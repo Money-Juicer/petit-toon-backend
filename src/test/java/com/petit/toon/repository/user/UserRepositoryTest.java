f19a5f6fd2caf33d27e73efdfeeb0528b0f48518
Hotoran <hotoran0410@gmail.com>
2023-09-12 23:32:20 +0900

    chore: PostgreSQL DB 변경에 따른 수정

--- src/test/java/com/petit/toon/repository/user/UserRepositoryTest.java
+++ src/test/java/com/petit/toon/repository/user/UserRepositoryTest.java
@@ -9,6 +9,7 @@ import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
@@ -25,6 +26,7 @@ import static org.assertj.core.api.Assertions.tuple;
 @DataJpaTest
 @ActiveProfiles("test")
 @Import({DefaultProfileImageSetup.class, QueryDslConfig.class})
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
